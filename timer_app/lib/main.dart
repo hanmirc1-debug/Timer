01e7f01f0df303b776c39eff75175a3575faf674
wjdgu <wjdgus221@naver.com>
2026-03-27 22:35:02 +0900

    wjdgus221 annotation added

--- timer_app/lib/main.dart
+++ timer_app/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const TimerPage(), // ⭐ 여기 중요
+      home: const TimerPage(), // ⭐ 여기 중요 wjdgus221
     );
   }
 }
